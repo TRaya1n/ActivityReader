665981abf74f6d9364a95bcb31f140d7840a1202
TRaya1n <138983011+TRaya1n@users.noreply.github.com>
2023-12-09 18:54:35 +0000

    Added logger

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,14 @@
 import { Bot } from "../libs/Client/index";
 import { GatewayIntentBits as Intents, Partials } from "discord.js";
+import { Logger } from "./functions/logger";
 
 const client = new Bot({
   intents: [Intents.Guilds, Intents.GuildMessages, Intents.MessageContent],
   partials: [Partials.GuildMember, Partials.Channel],
 });
+
+const logger = new Logger({ date: true });
+console.log = logger.log;
+console.error = logger.error;
 
 client.login();
---
+++ src/functions/logger.ts
@@ -0,0 +1,47 @@
+import chalk from "chalk";
+import moment from "moment";
+
+interface LoggerOptions {
+  date?: Boolean;
+}
+
+/**
+ * @returns String - time
+ */
+function getTime() {
+  return moment().utcOffset(+5).format("DD:HH:mm:ss");
+}
+
+export class Logger {
+  constructor(options: LoggerOptions) {
+    const date = options.date ? options.date : false;
+  }
+
+  /**
+   * @param{string} content
+   */
+  public log(content: string) {
+    console.warn(
+      chalk.bold.green(getTime()),
+      chalk.blueBright("[" + chalk.yellow("LOG") + "]"),
+      chalk.cyan(content),
+    );
+  }
+
+  /**
+   * @param{string} content
+   * @param{Error} error
+   */
+  public error(content: string, error: Error) {
+    console.warn(
+        chalk.bold.green(getTime()),
+        chalk.blueBright("[" + chalk.red('ERROR') + "]"), 
+        chalk.grey(content), "\n",
+        chalk.red(chalk.bold.underline.yellowBright('error?.message'), error.message), '\n',
+        chalk.red(chalk.bold.underline.yellowBright('error?.cause'), error?.cause || "null"), '\n',
+        chalk.red(chalk.bold.underline.yellowBright('error?.stack'), error?.stack || "null"), '\n',
+        chalk.yellow(error.name)
+    );
+  }
+  
+}
